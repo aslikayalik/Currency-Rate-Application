433428023753ffe27b7fba51be204bc25678fd47
ASLI KAYALIK <kayalik.asli@gmail.com>
2022-12-10 18:57:26 +0300

    Add project files.

---
+++ Core/BusinessLogicLayer.cs
@@ -0,0 +1,157 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Core
+{
+   public class BusinessLogicLayer:BaseClass
+    {
+        DatabaseLogicLayer DLL;
+        public BusinessLogicLayer()
+        {
+            DLL = new DatabaseLogicLayer();
+        }
+        public List<ParaBirimi> ParaBirimiListesi() 
+        {
+            List<ParaBirimi> ParaBirimleri = new List<ParaBirimi>(); 
+            SqlDataReader reader = DLL.ParaBirimiListesi();
+            while (reader.Read())
+            {
+                ParaBirimleri.Add(new ParaBirimi()
+                {
+                    ID = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                   
+                    Code = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
+                    Tanim = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
+                    UyariLimit = reader.IsDBNull(3) ? 0 : reader.GetDecimal(3)
+                });
+               
+
+            }
+            reader.Close();
+            DLL.Baglantıİslemleri();
+            return ParaBirimleri;
+        }
+
+   
+
+        public List<Kur> KurListe()
+        {
+            List<Kur> KurDeğerleri = new List<Kur>();
+
+            SqlDataReader reader = DLL.KurListe();
+            while (reader.Read())
+            {
+                KurDeğerleri.Add(new Kur()
+                { 
+                    ID = reader.IsDBNull(0) ? Guid.Empty:reader.GetGuid(0),
+                    ParaBirimiID = reader.IsDBNull(1) ? Guid.Empty : reader.GetGuid(1),
+                    Alis = reader.IsDBNull(2) ? 0 : reader.GetDecimal(2),
+                    Satis = reader.IsDBNull(3) ? 0 : reader.GetDecimal(3),
+                    OlusturmaTarih = reader.IsDBNull(4) ? DateTime.MinValue : reader.GetDateTime(4)
+
+                });
+            }
+            reader.Close();
+            DLL.Baglantıİslemleri();
+
+            return KurDeğerleri;
+        }
+
+
+        public Kur KurListe(Guid ParabirimiID) 
+        {
+            Kur Kur = new Kur(); 
+
+            SqlDataReader reader = DLL.KurListe();
+            while (reader.Read())   
+            {
+                Kur = new Kur() 
+                {
+                    ID = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                    ParaBirimiID = reader.IsDBNull(1) ? Guid.Empty : reader.GetGuid(1),
+                    Alis = reader.IsDBNull(2) ? 0 : reader.GetDecimal(2),
+                    Satis = reader.IsDBNull(3) ? 0 : reader.GetDecimal(3),
+                    OlusturmaTarih = reader.IsDBNull(4) ? DateTime.MinValue : reader.GetDateTime(4)
+
+                };
+            }
+            reader.Close();
+            DLL.Baglantıİslemleri();
+
+            return Kur;
+        }
+       
+        public List<KurGecmis> KurGecmisListe()
+        {
+            List<KurGecmis> KurGecmisListe = new List<KurGecmis>();
+            SqlDataReader reader = DLL.KurGecmisListe(); 
+
+            while (reader.Read())
+            {
+                KurGecmisListe.Add(new KurGecmis()
+                {
+                    ID=reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                    KurID=reader.IsDBNull(1) ? Guid.Empty : reader.GetGuid(1),
+                    ParaBirimiID = reader.IsDBNull(2) ? Guid.Empty : reader.GetGuid(2),
+                    Alis = reader.IsDBNull(3) ? 0 : reader.GetDecimal(3),
+                    Satis = reader.IsDBNull(4) ? 0 : reader.GetDecimal(4),
+                    OlusturmaTarih = reader.IsDBNull(5) ? DateTime.MinValue : reader.GetDateTime(5)
+
+
+                });
+            }
+            reader.Close();
+            DLL.Baglantıİslemleri();
+            return KurGecmisListe;
+        }
+
+
+        public List<KurGecmis> KurGecmisListe(Guid ParaBirimiID)
+        {
+            List<KurGecmis> KurGecmisListe = new List<KurGecmis>();
+            SqlDataReader reader = DLL.KurGecmisListe(ParaBirimiID); 
+            while (reader.Read())
+            {
+                KurGecmisListe.Add(new KurGecmis()
+                {
+                    ID = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                    KurID = reader.IsDBNull(1) ? Guid.Empty : reader.GetGuid(1),
+                    ParaBirimiID = reader.IsDBNull(2) ? Guid.Empty : reader.GetGuid(2),
+                    Alis = reader.IsDBNull(3) ? 0 : reader.GetDecimal(3),
+                    Satis = reader.IsDBNull(4) ? 0 : reader.GetDecimal(4),
+                    OlusturmaTarih = reader.IsDBNull(5) ? DateTime.MinValue : reader.GetDateTime(5)
+
+
+                });
+            }
+            reader.Close();
+            DLL.Baglantıİslemleri();
+            return KurGecmisListe;
+        }
+    
+       public void KurKayitEkle(Guid ID, Guid ParaBirimiID, decimal Alis, decimal Satis, DateTime OlusturmaTarih)
+        {
+            if(ID != Guid.Empty && ParaBirimiID != Guid.Empty && Alis != 0 && Satis != 0 && OlusturmaTarih > DateTime.MinValue)
+            {
+                
+                DLL.KurKayitEkle(new Kur()
+                { 
+                ID = ID,
+                ParaBirimiID=ParaBirimiID,
+                Alis=Alis,
+                Satis=Satis,
+                OlusturmaTarih=OlusturmaTarih
+
+                });;
+            }
+            else
+            {
+                
+            }
+            
+        }
+    }
+}
---
+++ WinApp/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_jsonkurgu = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_jsonkurgu
+            // 
+            this.btn_jsonkurgu.Location = new System.Drawing.Point(37, 41);
+            this.btn_jsonkurgu.Name = "btn_jsonkurgu";
+            this.btn_jsonkurgu.Size = new System.Drawing.Size(154, 23);
+            this.btn_jsonkurgu.TabIndex = 0;
+            this.btn_jsonkurgu.Text = "Json Data Çek ve Kaydet";
+            this.btn_jsonkurgu.UseVisualStyleBackColor = true;
+            this.btn_jsonkurgu.Click += new System.EventHandler(this.btn_jsonkurgu_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_jsonkurgu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_jsonkurgu;
+    }
+}
+
